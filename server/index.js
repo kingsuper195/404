19b86ccb4dbee74b72efb7497dd3976bf6a9d083
Peter Durdin <peter.durdin.superworld@gmail.com>
2025-06-18 21:02:59 +1000

    Add a goal and an even more annoying button.

--- server/index.js
+++ server/index.js
@@ -52,6 +52,10 @@ app.get('/home', (req, res) => {
     }
 });
 
+app.get('/button',(req,res)=>{
+    res.sendFile(path.join(__dirname, '/../public/button.html'));
+});
+
 app.get('/bootstrap', (req, res) => {
     res.sendFile(path.join(__dirname, '/../public/bootstrap.min.css'));
 });
